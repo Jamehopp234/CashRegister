d9e919921a898edc11127a34a51ee6c7dbde06da
jamehopp234 <jamehopp234@ed.amdsb.ca>
2021-09-30 13:54:55 -0400

    Add project files.

---
+++ CashRegister/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace CashRegister
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mildSausageLabel = new System.Windows.Forms.Label();
+            this.hotSausageLabel = new System.Windows.Forms.Label();
+            this.hotdogLabel = new System.Windows.Forms.Label();
+            this.mildSausagebox = new System.Windows.Forms.TextBox();
+            this.hotSausagebox = new System.Windows.Forms.TextBox();
+            this.hotdogBox = new System.Windows.Forms.TextBox();
+            this.costLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.totalPriceLabel = new System.Windows.Forms.Label();
+            this.paymentLabel = new System.Windows.Forms.Label();
+            this.paymentBox = new System.Windows.Forms.TextBox();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.buyButton = new System.Windows.Forms.Button();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.receiptButton = new System.Windows.Forms.Button();
+            this.receitLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mildSausageLabel
+            // 
+            this.mildSausageLabel.AutoSize = true;
+            this.mildSausageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mildSausageLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.mildSausageLabel.Location = new System.Drawing.Point(24, 57);
+            this.mildSausageLabel.Name = "mildSausageLabel";
+            this.mildSausageLabel.Size = new System.Drawing.Size(177, 31);
+            this.mildSausageLabel.TabIndex = 0;
+            this.mildSausageLabel.Text = "Mild Sausage";
+            this.mildSausageLabel.Click += new System.EventHandler(this.mildSausageLabel_Click);
+            // 
+            // hotSausageLabel
+            // 
+            this.hotSausageLabel.AutoSize = true;
+            this.hotSausageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hotSausageLabel.Location = new System.Drawing.Point(24, 120);
+            this.hotSausageLabel.Name = "hotSausageLabel";
+            this.hotSausageLabel.Size = new System.Drawing.Size(171, 31);
+            this.hotSausageLabel.TabIndex = 1;
+            this.hotSausageLabel.Text = "Hot Sausage";
+            // 
+            // hotdogLabel
+            // 
+            this.hotdogLabel.AutoSize = true;
+            this.hotdogLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hotdogLabel.Location = new System.Drawing.Point(24, 185);
+            this.hotdogLabel.Name = "hotdogLabel";
+            this.hotdogLabel.Size = new System.Drawing.Size(102, 31);
+            this.hotdogLabel.TabIndex = 2;
+            this.hotdogLabel.Text = "Hotdog";
+            // 
+            // mildSausagebox
+            // 
+            this.mildSausagebox.Location = new System.Drawing.Point(230, 68);
+            this.mildSausagebox.Name = "mildSausagebox";
+            this.mildSausagebox.Size = new System.Drawing.Size(66, 20);
+            this.mildSausagebox.TabIndex = 3;
+            // 
+            // hotSausagebox
+            // 
+            this.hotSausagebox.Location = new System.Drawing.Point(228, 134);
+            this.hotSausagebox.Name = "hotSausagebox";
+            this.hotSausagebox.Size = new System.Drawing.Size(67, 20);
+            this.hotSausagebox.TabIndex = 4;
+            // 
+            // hotdogBox
+            // 
+            this.hotdogBox.Location = new System.Drawing.Point(225, 196);
+            this.hotdogBox.Name = "hotdogBox";
+            this.hotdogBox.Size = new System.Drawing.Size(69, 20);
+            this.hotdogBox.TabIndex = 5;
+            // 
+            // costLabel
+            // 
+            this.costLabel.AutoSize = true;
+            this.costLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.costLabel.Location = new System.Drawing.Point(34, 318);
+            this.costLabel.Name = "costLabel";
+            this.costLabel.Size = new System.Drawing.Size(56, 25);
+            this.costLabel.TabIndex = 6;
+            this.costLabel.Text = "Cost";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxLabel.Location = new System.Drawing.Point(34, 358);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(48, 25);
+            this.taxLabel.TabIndex = 7;
+            this.taxLabel.Text = "Tax";
+            // 
+            // totalPriceLabel
+            // 
+            this.totalPriceLabel.AutoSize = true;
+            this.totalPriceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalPriceLabel.Location = new System.Drawing.Point(34, 396);
+            this.totalPriceLabel.Name = "totalPriceLabel";
+            this.totalPriceLabel.Size = new System.Drawing.Size(115, 25);
+            this.totalPriceLabel.TabIndex = 8;
+            this.totalPriceLabel.Text = "Total Price";
+            // 
+            // paymentLabel
+            // 
+            this.paymentLabel.AutoSize = true;
+            this.paymentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.paymentLabel.Location = new System.Drawing.Point(34, 431);
+            this.paymentLabel.Name = "paymentLabel";
+            this.paymentLabel.Size = new System.Drawing.Size(172, 25);
+            this.paymentLabel.TabIndex = 9;
+            this.paymentLabel.Text = "Pay Money Here";
+            // 
+            // paymentBox
+            // 
+            this.paymentBox.Location = new System.Drawing.Point(222, 431);
+            this.paymentBox.Name = "paymentBox";
+            this.paymentBox.Size = new System.Drawing.Size(74, 20);
+            this.paymentBox.TabIndex = 10;
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.AutoSize = true;
+            this.changeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeLabel.Location = new System.Drawing.Point(48, 543);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(87, 25);
+            this.changeLabel.TabIndex = 11;
+            this.changeLabel.Text = "Change";
+            this.changeLabel.Click += new System.EventHandler(this.changeLabel_Click);
+            // 
+            // buyButton
+            // 
+            this.buyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buyButton.Location = new System.Drawing.Point(53, 252);
+            this.buyButton.Name = "buyButton";
+            this.buyButton.Size = new System.Drawing.Size(153, 35);
+            this.buyButton.TabIndex = 12;
+            this.buyButton.Text = "Buy Items";
+            this.buyButton.UseVisualStyleBackColor = true;
+            this.buyButton.Click += new System.EventHandler(this.buyButton_Click);
+            // 
+            // changeButton
+            // 
+            this.changeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeButton.Location = new System.Drawing.Point(39, 493);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(201, 36);
+            this.changeButton.TabIndex = 13;
+            this.changeButton.Text = "Calculate Change";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // receiptButton
+            // 
+            this.receiptButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptButton.Location = new System.Drawing.Point(43, 585);
+            this.receiptButton.Name = "receiptButton";
+            this.receiptButton.Size = new System.Drawing.Size(163, 32);
+            this.receiptButton.TabIndex = 14;
+            this.receiptButton.Text = "Print Receipt";
+            this.receiptButton.UseVisualStyleBackColor = true;
+            this.receiptButton.Click += new System.EventHandler(this.receiptButton_Click);
+            // 
+            // receitLabel
+            // 
+            this.receitLabel.AutoSize = true;
+            this.receitLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receitLabel.Location = new System.Drawing.Point(485, 83);
+            this.receitLabel.Name = "receitLabel";
+            this.receitLabel.Size = new System.Drawing.Size(0, 25);
+            this.receitLabel.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 629);
+            this.Controls.Add(this.receitLabel);
+            this.Controls.Add(this.receiptButton);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.buyButton);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.paymentBox);
+            this.Controls.Add(this.paymentLabel);
+            this.Controls.Add(this.totalPriceLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.costLabel);
+            this.Controls.Add(this.hotdogBox);
+            this.Controls.Add(this.hotSausagebox);
+            this.Controls.Add(this.mildSausagebox);
+            this.Controls.Add(this.hotdogLabel);
+            this.Controls.Add(this.hotSausageLabel);
+            this.Controls.Add(this.mildSausageLabel);
+            this.Name = "Form1";
+            this.Text = "The Hot Diggity Dawg House";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mildSausageLabel;
+        private System.Windows.Forms.Label hotSausageLabel;
+        private System.Windows.Forms.Label hotdogLabel;
+        private System.Windows.Forms.TextBox mildSausagebox;
+        private System.Windows.Forms.TextBox hotSausagebox;
+        private System.Windows.Forms.TextBox hotdogBox;
+        private System.Windows.Forms.Label costLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label totalPriceLabel;
+        private System.Windows.Forms.Label paymentLabel;
+        private System.Windows.Forms.TextBox paymentBox;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Button buyButton;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Button receiptButton;
+        private System.Windows.Forms.Label receitLabel;
+    }
+}
+
---
+++ CashRegister/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace CashRegister
+{
+    public partial class Form1 : Form
+    {
+        int mild = 6;
+        int hot = 6;
+        int dog = 5;
+        int mildBox;
+        int hotBox;
+        int dogBox;
+        int mildPrice;
+        int hotPrice;
+        int dogPrice;
+        double totalCost;
+        double finalCost;
+        double tax = 0.13;
+        double finalTax;
+        double change;
+        int payment;
+        
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buyButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                receitLabel.Text = $"";
+                mildBox = Convert.ToInt32(mildSausagebox.Text);
+                hotBox = Convert.ToInt32(hotSausagebox.Text);
+                dogBox = Convert.ToInt32(hotdogBox.Text);
+
+                mildPrice = mildBox * mild;
+                hotPrice = hotBox * hot;
+                dogPrice = dogBox * dog;
+                
+                totalCost = mildPrice + hotPrice + dogPrice;
+                finalTax = totalCost * tax;
+                finalCost = totalCost + tax;
+
+                costLabel.Text = $"Cost: {totalCost.ToString("C")}";
+                taxLabel.Text = $"Tax: {finalTax.ToString("C")}";
+                totalPriceLabel.Text = $"Total price: {finalCost.ToString("c")}";
+            }
+            catch
+            {
+                receitLabel.Text = $"Please put only whole numbers in the boxes";
+            }
+            
+        }
+
+        private void mildSausageLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void changeButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                receitLabel.Text = $"";
+                totalPriceLabel.Text = $"Total price: {finalCost.ToString("c")}";
+                payment = Convert.ToInt32(paymentBox.Text);
+                change = payment - finalCost;
+                changeLabel.Text = $"Change: {change.ToString("c")}";
+            }
+            catch
+            {
+                receitLabel.Text = $"We only accept full bills here";
+            }
+        }
+
+        private void changeLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void receiptButton_Click(object sender, EventArgs e)
+        {
+            payment = Convert.ToInt32(paymentBox.Text);
+            receitLabel.Text += $"\n\nHot Diggity Dawg House";
+            Refresh();
+            Thread.Sleep(500);
+            receitLabel.Text += $"\nOrder Number 05";
+            Refresh();
+            Thread.Sleep(500);
+            receitLabel.Text += $"\nSeptember 30 2021";
+            Refresh();
+            Thread.Sleep(500);
+            receitLabel.Text += $"\nMild Sausage  x{mildBox} @ {mildPrice.ToString("c")}";
+            Refresh();
+            Thread.Sleep(500);
+            receitLabel.Text += $"\nHot Sausage   x{hotBox} @ {hotPrice.ToString("c")}";
+            Refresh();
+            Thread.Sleep(500);
+            receitLabel.Text += $"\nHotDog        x{dogBox} @ {dogPrice.ToString("c")}";
+            Refresh();
+            Thread.Sleep(500);
+            receitLabel.Text += $"\nSubtotal                  {totalCost.ToString("c")}";
+            Refresh();
+            Thread.Sleep(500);
+            receitLabel.Text += $"\nTax                       {tax.ToString("c")}";
+            Refresh();
+            Thread.Sleep(500);
+            receitLabel.Text += $"\nTotal                     {finalCost.ToString("c")}";
+            Refresh();
+            Thread.Sleep(500);
+            receitLabel.Text += $"\nTendered                  {payment.ToString("c")}";
+            Refresh();
+            Thread.Sleep(500);
+            receitLabel.Text += $"\nChange                    {change.ToString("c")}";
+            Refresh();
+            Thread.Sleep(500);
+            receitLabel.Text += $"\nHave a nice day!";
+
+
+
+            ;        }
+
+        private void mildReceipt_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
