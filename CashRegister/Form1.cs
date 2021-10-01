7804b20e07c915f7a763762034f5d79d202f8f5d
jamehopp234 <jamehopp234@ed.amdsb.ca>
2021-10-01 14:20:15 -0400

    final draft

--- CashRegister/Form1.cs
+++ CashRegister/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//Author: James Hopper
+//Purpose: A cash register able to calculate price with tax of three items, and able to calculate change and print a receipt
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,11 +11,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using System.Media;
 
 namespace CashRegister
 {
     public partial class Form1 : Form
     {
+        //These are all my variables
         int mild = 6;
         int hot = 6;
         int dog = 5;
@@ -40,26 +45,33 @@ namespace CashRegister
         {
             try
             {
+                //This makes sure the catch label is blank
                 receitLabel.Text = $"";
+
+                //This converts the text boxes to intigers
                 mildBox = Convert.ToInt32(mildSausagebox.Text);
                 hotBox = Convert.ToInt32(hotSausagebox.Text);
                 dogBox = Convert.ToInt32(hotdogBox.Text);
 
+                //setting what the price variables equal
                 mildPrice = mildBox * mild;
                 hotPrice = hotBox * hot;
                 dogPrice = dogBox * dog;
                 
+                //calculaing the total price, tax, and the final price with tax included
                 totalCost = mildPrice + hotPrice + dogPrice;
                 finalTax = totalCost * tax;
                 finalCost = totalCost + tax;
 
+                //Tells the cost, tax, and total price label what to say
                 costLabel.Text = $"Cost: {totalCost.ToString("C")}";
                 taxLabel.Text = $"Tax: {finalTax.ToString("C")}";
                 totalPriceLabel.Text = $"Total price: {finalCost.ToString("c")}";
             }
             catch
             {
-                receitLabel.Text = $"Please put only whole numbers in the boxes";
+                //If someone puts a letter or decimal in the text box this will display
+                receitLabel.Text = $"We sell food here not portions or letters.";
             }
             
         }
@@ -73,14 +85,22 @@ namespace CashRegister
         {
             try
             {
+                //reset in case the typed a letter or decimal 
                 receitLabel.Text = $"";
                 totalPriceLabel.Text = $"Total price: {finalCost.ToString("c")}";
+                
+                //setting payment textbox to an intager
                 payment = Convert.ToInt32(paymentBox.Text);
+
+                //setting what the variables equal
                 change = payment - finalCost;
+
+                //telling what the change label to say
                 changeLabel.Text = $"Change: {change.ToString("c")}";
             }
             catch
             {
+                //if someone puts letters or decimals in the text box display this
                 receitLabel.Text = $"We only accept full bills here";
             }
         }
@@ -92,7 +112,17 @@ namespace CashRegister
 
         private void receiptButton_Click(object sender, EventArgs e)
         {
+            //reset receipt label in case som typed a letter or decimal in the text box
+            receitLabel.Text = $"";
+
+            //setting the payment box to a intager
             payment = Convert.ToInt32(paymentBox.Text);
+
+            //playing the receipt sound
+            SoundPlayer soundPlayer = new SoundPlayer(Properties.Resources.reccepit);
+            soundPlayer.Play();
+
+            //printing the receipt
             receitLabel.Text += $"\n\nHot Diggity Dawg House";
             Refresh();
             Thread.Sleep(500);
